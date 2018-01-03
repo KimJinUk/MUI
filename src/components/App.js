bc5c3f9cf11a639b38cc311040adb596e171f13b
jukim <jik1423248@gmail.com>
2018-01-03 23:58:55 +0900

    second init

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppBarTemplete from './AppBarTemplete';
+
+class App extends React.Component {
+    render(){
+
+        return (
+                <div>
+                		<AppBarTemplete/>
+                </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/AppBarTemplete.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+
+class Login extends Component {
+
+	constructor(props) {
+		super(props)
+
+		let muiName = 'FlatButton';	
+	}
+	
+
+	render() {
+		return(
+			<FlatButton {...this.props} label="Login"/>
+		);
+	}
+}
+
+const Logged = (props) => (
+  <IconMenu
+    {...props}
+    iconButtonElement={
+      	<IconButton>
+      		<MoreVertIcon />
+      	</IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <MenuItem primaryText="Refresh" />
+    <MenuItem primaryText="Help" />
+    <MenuItem primaryText="Sign out" />
+  </IconMenu>
+);
+
+Logged.muiName = 'IconMenu';
+
+class AppBarTemplete extends Component {
+
+	constructor(props) {
+		super(props)
+
+		this.state = {
+    		logged: true,
+  		};
+	}
+
+  	render() {
+  		return (
+  			<MuiThemeProvider>
+					<AppBar
+						title="To do List"
+						iconElementRight={this.state.logged ? <Logged /> : <Login />}
+						style={{textAlign:"center"}}
+					/>
+			</MuiThemeProvider>
+  		)
+  	}
+}
+
+export default AppBarTemplete;
